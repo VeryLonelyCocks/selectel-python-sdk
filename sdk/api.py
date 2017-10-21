fbcaf134e628f214272390142d0bd514bff51d2a
Anton Klochkov <tklochkov@gmail.com>
2017-10-21 15:53:52 +0300

    Refactored constants

--- sdk/api.py
+++ sdk/api.py
@@ -2,8 +2,8 @@ import requests
 
 
 class API:
-    cloud_storage_URL = 'https://api.selcdn.ru/'
-    vpc_URL = 'https://api.selectel.ru/vpc/resell/v2/'
+    CS_URL = 'https://api.selcdn.ru/'
+    VPC_URL = 'https://api.selectel.ru/vpc/resell/v2/'
 
     def request(self, url, headers=None, params=None, method='GET', files=None):
 
