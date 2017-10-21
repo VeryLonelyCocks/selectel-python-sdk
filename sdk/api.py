ce3c305bfeeea1c15447b7b25aeb3bd54240cb7c
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-10-21 23:52:55 +0300

    Updates

--- sdk/api.py
+++ sdk/api.py
@@ -4,11 +4,11 @@ import requests
 class API:
     URL = 'https://api.selcdn.ru/'
 
-    def request(self, url, headers=None, params=None, method='GET', files=None):
+    def request(self, url, headers=None, params=None, method='GET', files=None, stream=False):
 
         if headers is None:
             headers = {}
         if params is None:
             params = {}
 
-        return requests.request(method, url, headers=headers, params=params, files=files)
+        return requests.request(method, url, headers=headers, params=params, files=files, stream=stream)
