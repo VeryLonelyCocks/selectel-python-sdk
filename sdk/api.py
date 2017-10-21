9585ffe39234414e84cf838238377b42c104d138
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-10-21 03:48:20 +0300

    Initial

---
+++ sdk/api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class API:
+    URL = 'https://api.selcdn.ru/'
+
+    def request(self, url, headers=None, params=None, method='GET', files=None):
+
+        if headers is None:
+            headers = {}
+        if params is None:
+            params = {}
+
+        return requests.request(method, url, headers=headers, params=params, files=files)
