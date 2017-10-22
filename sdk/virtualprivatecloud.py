ab8ed5201a40472a4dd40d200e59605803ba9628
Anton Klochkov <tklochkov@gmail.com>
2017-10-22 03:11:32 +0300

    Added get stat data

--- sdk/virtualprivatecloud.py
+++ sdk/virtualprivatecloud.py
@@ -1,4 +1,7 @@
 from .api import API
+import json
+import requests
+import datetime
 
 
 class VirtualPrivateCloud(API):
@@ -72,3 +75,60 @@ class VirtualPrivateCloud(API):
         response = self._send_request(url)
 
         return response.json()
+
+    def get_token(self):
+        url = self.VPC_URL + 'tokens'
+
+        data = {"token": {"project_id": "969218765ec44cce94e127c49ea64752"}}
+        q = requests.post(url, data=json.dumps(data),
+                          headers={'X-token': self._token,'Content-type': 'application/json'})
+
+        self._auth_token = q.json()['token']['id']
+
+        return self._auth_token
+
+    def get_list_subnets(self):
+        url = self.VPC_URL + "subnets"
+
+        response = self._send_request(url)
+
+        return response.json()
+
+    def get_info_subnet(self, subnet_id):
+        url = self.VPC_URL + "subnets/" + str(subnet_id)
+
+        response = self._send_request(url)
+
+        return response.json()
+
+    def _get_statistic(self, server_id, url_paste):
+
+        if not hasattr(self, '_auth_token'):
+            self.get_token()
+
+        today = datetime.datetime.now() - datetime.timedelta(hours=3) - datetime.timedelta(minutes=2)
+        yesterday = (today - datetime.timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:00.000Z")
+        today = today.strftime("%Y-%m-%dT%H:%M:00.000Z")
+
+        url = "https://api.selvpc.ru/metric/v1/resource/sel_instance/{server_id}/metric/{url_paste}/measures?" \
+              "granularity=300&start={start}&stop={stop}".replace(" ", "").format(server_id=server_id,
+                                                                                  start=yesterday,
+                                                                                  stop=today,
+                                                                                  url_paste=url_paste)
+
+        headers = {
+            'X-Auth-Token': self._auth_token,
+            'Content-Type': 'application/json'
+        }
+
+        response = self.request(url, headers=headers, method="GET")
+
+        return response.json()
+
+    def get_cpu_data(self, server_id):
+
+        return self._get_statistic(server_id, "cpu_util")
+
+    def get_mem_data(self, server_id):
+
+        return self._get_statistic(server_id, "memory.usage")
